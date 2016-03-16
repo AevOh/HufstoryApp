e819f85a07b0e978f55c2f2e8e847c241ab4520e
wook <guddnr609@naver.com>
2016-03-16 20:55:48 +0900

    merge

--- NavigationDrawer/app/src/main/java/co/kr/hufstory/main/MainActivity.java
+++ NavigationDrawer/app/src/main/java/co/kr/hufstory/main/MainActivity.java
@@ -115,7 +115,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        new CountDownTimer(3000, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {}
+
+            @Override
+            public void onFinish() {
+                MainActivity.this.setContentView(R.layout.activity_main);
+            }
+        }.start();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
 
