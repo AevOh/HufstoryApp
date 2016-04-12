b2fd948e78b902ec4466c3b4eb097ee842f22949
wook <guddnr609@naver.com>
2016-04-12 12:06:53 +0900

    기울어짐 버그 수정, 게시판 잘못된 이름 수정

--- NavigationDrawer/app/src/main/java/co/kr/hufstory/main/MainActivity.java
+++ NavigationDrawer/app/src/main/java/co/kr/hufstory/main/MainActivity.java
@@ -116,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
